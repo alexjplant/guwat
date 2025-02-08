98ef07b9386d5d681292a3dba33ad02680168f17
Alex Plant <alex@alexplant.org>
2025-02-07 20:19:33 -0800

    Init go hello world and some swaggo

---
+++ cmd/api/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	http.ListenAndServe(":8080", handler)
+}

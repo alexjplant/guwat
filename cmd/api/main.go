c091868881253c87e27a186a3b9a1df31bff7755
Alex Plant <alex@alexplant.org>
2025-02-12 17:04:20 -0800

    DB connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
+	db, err := sql.Open("pgx", "postgres://username:password@localhost:5432/database?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
